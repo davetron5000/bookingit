85a4aefbe753dc2d6d0baba6ac06c45027a68178
Doug Forster <douglas.p.forster@gmail.com>
2014-11-28 20:53:27 -0800

    Before running commands, ensure that the environment of the subshell is properly configured by: sourcing RVM; unsetting the BUNDLE_GEMFILE environment variable; and calling 'rvm use .' to set up the receta gemset.

--- lib/bookingit/shell_command.rb
+++ lib/bookingit/shell_command.rb
@@ -19,7 +19,21 @@ module Bookingit
 
     def run!
       chdir @path do
-        @stdout, @stderr, status = Open3.capture3(@command)
+        bash_script = <<SCRIPT
+# Ensure that RVM is properly loaded in the new subshell
+source "$HOME/.rvm/scripts/rvm"
+unset BUNDLE_GEMFILE
+
+# Tell RVM to use the gemset defined in .ruby-gemset and .ruby-version (e.g. : 2.1.0@receta)
+# and to set up environment variables within the new subshell.  This call to 'rvm use'
+# is necessary to avoid inheriting RVM environment variables from the calling shell
+# (e.g. 2.1.0@angular-rails-book)
+rvm use . > /dev/null 2>&1
+
+#{@command}
+SCRIPT
+
+        @stdout, @stderr, status = Open3.capture3("bash", stdin_data: bash_script)
         @exit_code = status.exitstatus
       end
       unless @exit_status_checker.(@exit_code)
